d9220f7889750a11db7a11d426bcf8ccb0a27fbd
Deodate <ajadeo04@gmail.com>
2025-02-10 20:40:52 +0200

    PROGRESS

---
+++ contacts/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:contacts_service/contacts_service.dart';
+import 'package:flutter/material.dart';
+import 'mock_contacts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CONTACTS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Kenyan Contacts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Contact> contacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getAllContacts();
+  }
+
+  getAllContacts() {
+    setState(() {
+      contacts = MockContactsData.getMockContacts();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                Contact contact = contacts[index];
+                return ListTile(
+                  title: Text(contact.displayName ?? 'No Name'),
+                  subtitle: Text(
+                    contact.phones?.isNotEmpty == true 
+                        ? contact.phones!.first.value ?? 'No Number'
+                        : 'No Number'
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
